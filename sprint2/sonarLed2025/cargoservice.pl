a716d0c3300fda39ca8bef482c63aaacbbd054fb
MarcoCrisafulli5 <marcocrisa2001@gmail.com>
2026-02-28 12:23:54 +0100

    sonarOnrasp

---
+++ sprint2/sonarLed2025/cargoservice.pl
@@ -0,0 +1,14 @@
+%====================================================================================
+% cargoservice description   
+%====================================================================================
+mqttBroker("192.168.1.10", "1883", "sonardata").
+event( sonardata, distance(D) ).
+event( unibologprolog, unibologprolog(SOURCE,CATEG,CONTENT) ).
+dispatch( doblink, doblink(X) ).
+dispatch( ledOn, ledOn(X) ).
+%====================================================================================
+context(ctxsonarled2025, "localhost",  "TCP", "8128").
+ qactor( mind, ctxsonarled2025, "it.unibo.mind.Mind").
+ static(mind).
+  qactor( sonardevice, ctxsonarled2025, "it.unibo.sonardevice.Sonardevice").
+ static(sonardevice).
